573d948dedf17a519fcf8977129865eb56e723c2
keshavapatteri <mktkid2023@gmail.com>
2025-05-03 21:35:18 +0530

    Re-alll

--- src/Config/AxiosInstance.js
+++ src/Config/AxiosInstance.js
@@ -11,10 +11,20 @@
 // console.log(`api`,`${import.meta.env.VITEAPI}`);
 
 
+// import axios from "axios";
+
+
+// export const axiosInstance = axios.create({
+//     headers:{authorization:localStorage.getItem("User")?.accesToken},
+//     baseURL:`${import.meta.env.VITEAPI}/api/v1`
+// });
+
+
 import axios from "axios";
 
-
 export const axiosInstance = axios.create({
-    headers:{authorization:localStorage.getItem("User")?.accesToken},
-    baseURL:`${import.meta.env.VITEAPI}/api/v1`
+    baseURL: `http://localhost:3005/v1`,
+    withCredentials: true,
 });
+
+console.log(`api`, `${import.meta.env.VITEAPI}`);

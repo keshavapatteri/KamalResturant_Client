f712804d0303a28e5094e897f44210ad22171550
keshavapatteri <mktkid2023@gmail.com>
2025-05-03 21:32:02 +0530

    Re-aaaa

--- src/Config/AxiosInstance.js
+++ src/Config/AxiosInstance.js
@@ -1,11 +1,20 @@
+// import axios from "axios";
+
+// export const axiosInstance= axios.create({
+//     baseURL: `http://localhost:3005/v1`,
+//     withCredentials:true,
+    
+// }
+
+
+// )
+// console.log(`api`,`${import.meta.env.VITEAPI}`);
+
+
 import axios from "axios";
 
-export const axiosInstance= axios.create({
-    baseURL: `http://localhost:3005/v1`,
-    withCredentials:true,
-    
-}
 
-
-)
-console.log(`api`,`${import.meta.env.VITEAPI}`);
+export const axiosInstance = axios.create({
+    headers:{authorization:localStorage.getItem("User")?.accesToken},
+    baseURL:`${import.meta.env.VITEAPI}/api/v1`
+});
